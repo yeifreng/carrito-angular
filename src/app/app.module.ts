1bb18fef120d6fde8ceccf99f4ec78dffcb7f905
Yeifreng <yeifrenjose@gmail.com>
2023-10-31 19:11:38 -0500

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './productos/productos.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
